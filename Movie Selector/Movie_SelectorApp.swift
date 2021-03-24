7ccf026847acc720285e33d7e50f771d2a35ffd4
Andreas Velounias <>
2021-03-24 13:35:34 +0000

    Create new Xcode project

---
+++ Movie Selector/Movie_SelectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_SelectorApp.swift
+//  Movie Selector
+//
+//  Created by Andreas Velounias on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Movie_SelectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
